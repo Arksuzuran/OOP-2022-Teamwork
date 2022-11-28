25679c2ef4d19f7c87197819b2e9e1db2d3bdcc2
czx <2471280802@qq.com>
2022-11-28 18:03:12 +0800

    test03 add k

--- src/main/java/com/example/teamproject/HelloApplication.java
+++ src/main/java/com/example/teamproject/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         int i = 0;
-        int j = 1;
+        int k = 2;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
