079df5d78604f4fd652d5e72e0a26f407d2fada9
czx <2471280802@qq.com>
2022-11-28 19:20:10 +0800

    test04 add fx

--- src/main/java/com/example/teamproject/HelloApplication.java
+++ src/main/java/com/example/teamproject/HelloApplication.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        int i = 0;
-        int j = 1;
+        int fx = 4;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
